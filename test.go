de4af5e46ee950c964867d2690a77a564ee6d640
Jonny <ji394su311203106@gmail.com>
2024-02-02 22:59:10 +0800

    test for cherry pick

--- test.go
+++ test.go
@@ -4,3 +4,17 @@ func test() {
 	a := 12345
 	println(a)
 }
+
+
+=> feature/test -> commit ABC ->
+=> feature/test123 -> commit ABD -> merge main ->  ABD
+
+
+
+feature/test -> commit ABD -> commit ABC123 -> pull from main ABD  -> ABC123
+
+
+feature/line -> media ABC
+feature/facebook -> media ABC
+
+-> interface media
